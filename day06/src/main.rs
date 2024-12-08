111b6f2467afa1751c26edbe899e5806b7bba6a7
Anton Danneels <antondanneels@hotmail.com>
2024-12-08 20:29:34 +0100

    day06: rework to semi-bruteforce part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 enum Direction {
     Up,
     Right,
@@ -9,7 +9,7 @@ enum Direction {
 }
 
 fn main() {
-    let mut position = (-1, -1);
+    let mut initial_position = (-1, -1);
     let mut direction = Direction::Up;
     let map: Vec<Vec<bool>> = include_str!("input1.txt")
         .lines()
@@ -22,7 +22,7 @@ fn main() {
                         return true;
                     }
                     if c == '^' {
-                        position = (x as i32, y as i32);
+                        initial_position = (x as i32, y as i32);
                     }
 
                     false
@@ -31,12 +31,10 @@ fn main() {
         })
         .collect();
 
-    let mut visited: HashMap<(i32, i32), bool> = HashMap::new();
-    let mut previous_points = Vec::new();
-    let mut loops = 0;
-    let mut loop_points: HashMap<(i32, i32), bool> = HashMap::new();
+    let mut visited: HashMap<(i32, i32), Direction> = HashMap::new();
+    let mut position = initial_position;
     loop {
-        visited.insert(position.clone(), true);
+        visited.insert(position.clone(), direction.clone());
 
         let (next_position, next_direction) = match direction {
             Direction::Up => ((position.0, position.1 - 1), Direction::Right),
@@ -55,125 +53,57 @@ fn main() {
 
         if map[next_position.1 as usize][next_position.0 as usize] {
             direction = next_direction;
-            previous_points.push(next_position);
         } else {
-
-            // use all but the last three positions as we need to form a square
-            let selected_points = previous_points.iter()
-                                                 .enumerate()
-                                                 .filter(|(i, _)| (*i as i32) < previous_points.len() as i32 - 2)
-                                                 .map(|(_, e)| e);
-
-            // if we have a direct path to a previous node from the current position, we have a
-            // potential loop
-            for previous_point in selected_points {
-                // match next direction as we need to turn towards the point
-                match next_direction {
-                    Direction::Up => {
-                        if previous_point.0 != position.0 || previous_point.1 >= position.1 {
-                            continue;
-                        }
-
-                        let mut direct_line = true;
-                        for y in (previous_point.1 + 1)..(position.1) {
-                            if map[y as usize][position.0 as usize] {
-                                direct_line = false;
-                                break;
-                            }
-                        };
-
-                        if direct_line {
-                            loops += 1;
-                            loop_points.insert((position.0 - 1, position.1), true);
-                        }
-                    },
-                    Direction::Right => {
-                        if previous_point.1 != position.1 || previous_point.0 <= position.0 {
-                            continue;
-                        }
-
-                        let mut direct_line = true;
-                        for x in (position.0)..(previous_point.0) {
-                            if map[position.1 as usize][x as usize] {
-                                direct_line = false;
-                                break;
-                            }
-                        };
-
-                        if direct_line {
-                            loops += 1;
-                            loop_points.insert((position.0, position.1 - 1), true);
-                        }
-                    },
-                    Direction::Down => {
-                        if previous_point.0 != position.0 || previous_point.1 <= position.1 {
-                            continue;
-                        }
-
-                        let mut direct_line = true;
-                        for y in (position.1)..(previous_point.1) {
-                            if map[y as usize][position.0 as usize] {
-                                direct_line = false;
-                                break;
-                            }
-                        };
-
-                        if direct_line {
-                            loops += 1;
-                            loop_points.insert((position.0 + 1, position.1), true);
-                        }
-                    },
-                    Direction::Left => {
-                        if previous_point.1 != position.1 || previous_point.0 >= position.0 {
-                            continue;
-                        }
-
-                        let mut direct_line = true;
-                        for x in (previous_point.0 + 1)..(position.0) {
-                            if map[position.1 as usize][x as usize] {
-                                direct_line = false;
-                                break;
-                            }
-                        };
-
-                        if direct_line {
-                            loops += 1;
-                            loop_points.insert((position.0, position.1 + 1), true);
-                        }
-                    },
-                }
-            }
             position = next_position;
         }
     }
-    for (y, row) in map.iter().enumerate() {
-        for (x, _) in row.iter().enumerate() {
-            if map[y][x] {
-                print!("#");
+
+    let mut blocks = Vec::new();
+    for (extra_obstacle, _) in visited.iter() {
+        if *extra_obstacle == initial_position {
+            continue;
+        }
+        let mut new_visited: HashMap<(i32, i32), Vec<Direction>> = HashMap::new();
+        position = initial_position;
+        direction = Direction::Up;
+        loop {
+            let mut found_block = false;
+            let directions = new_visited.entry(position).or_insert(Vec::new());
+            for old_direction in directions.iter() {
+                if *old_direction == direction {
+                    blocks.push(extra_obstacle.clone());
+                    found_block = true;
+                    break;
+                }
+            }
+            if found_block {
+                break;
+            }
+            directions.push(direction.clone());
+
+            let (next_position, next_direction) = match direction {
+                Direction::Up => ((position.0, position.1 - 1), Direction::Right),
+                Direction::Right => ((position.0 + 1, position.1), Direction::Down),
+                Direction::Down => ((position.0, position.1 + 1), Direction::Left),
+                Direction::Left => ((position.0 - 1, position.1), Direction::Up),
+            };
+
+            if next_position.0 < 0
+                || next_position.0 >= map[0].len() as i32
+                || next_position.1 < 0
+                || next_position.1 >= map.len() as i32
+            {
+                break;
+            }
+
+            if map[next_position.1 as usize][next_position.0 as usize] ||
+                next_position.0 == extra_obstacle.0 && next_position.1 == extra_obstacle.1 {
+                direction = next_direction;
             } else {
-                print!(".");
+                position = next_position;
             }
         }
-        println!();
     }
 
-    println!();
-
-    for (y, row) in map.iter().enumerate() {
-        for (x, _) in row.iter().enumerate() {
-            if loop_points.contains_key(&(x as i32, y as i32)) {
-                print!("O");
-            } else if map[y][x] {
-                print!("#");
-            } else if visited.contains_key(&(x as i32, y as i32)) {
-                print!("x");
-            } else {
-                print!(".");
-            }
-        }
-        println!();
-    }
-
-    println!("{:?}", visited.len());
-    println!("{:?}", loops);
+    println!("{:?}", blocks.len());
 }
