23343d45e851437a7d762fbbdd496d3f6361e0d1
Anton Danneels <antondanneels@hotmail.com>
2024-12-30 00:04:13 +0100

    day23: naive way of generating every cluster

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,25 +1,26 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
     let mut links = HashMap::new();
+    let mut clusters = VecDeque::new();
 
-    include_str!("input.txt")
-        .lines()
-        .for_each(|line| {
-            let parts: Vec<&str> = line.split("-").map(|part| part.trim()).collect();
+    include_str!("input.txt").lines().for_each(|line| {
+        let parts: Vec<&str> = line.split("-").map(|part| part.trim()).collect();
 
-            let l = links.entry(parts[0]).or_insert(Vec::new());
-            l.push(parts[1]);
-            let l = links.entry(parts[1]).or_insert(Vec::new());
-            l.push(parts[0]);
-        });
+        let l = links.entry(parts[0]).or_insert(Vec::new());
+        l.push(parts[1]);
+        let l = links.entry(parts[1]).or_insert(Vec::new());
+        l.push(parts[0]);
 
-    println!("{:?}", links);
+        clusters.push_back(Vec::from([parts[0], parts[1]]));
+    });
+
+    println!("{:?}", links.len());
 
     //println!("{} {:?}", link, neighbours);
     //let mut unique_clusters = HashSet::new();
     for (link, neighbours) in links.iter() {
-        println!("{:?}", neighbours.len());
+        //println!("{:?}", neighbours.len());
         if link.starts_with("t") {
             /*
             for i in 0..neighbours.len() {
@@ -37,5 +38,68 @@ fn main() {
         }
     }
 
-    //println!("{:?}", unique_clusters.len());
+    let mut processed_clusters = Vec::new();
+    let mut memory = HashSet::new();
+    loop {
+        let mut change = false;
+        let cluster = match clusters.pop_front() {
+            Some(x) => x,
+            None => {
+                break;
+            }
+        };
+
+        //println!("{:?}", cluster);
+
+        let mut common = Vec::new();
+        for (i, l) in cluster.iter().enumerate() {
+            if i == 0 {
+                common = links.get(l).unwrap().clone();
+            } else {
+                common.retain(|x| links.get(l).unwrap().contains(x) && !cluster.contains(x));
+            }
+
+            //println!("{:?}", common);
+        }
+
+        //println!("Common neighbours: {:?}", common);
+        //let line = std::io::stdin().lines().next().unwrap().unwrap();
+
+        for e in common.iter() {
+            let mut new_cluster = cluster.clone();
+            new_cluster.push(e);
+            new_cluster.sort();
+            if !memory.contains(&new_cluster) {
+                clusters.push_back(new_cluster.clone());
+                memory.insert(new_cluster);
+                change = true;
+            }
+        }
+
+        if !change {
+            processed_clusters.push(cluster);
+        }
+    }
+
+    println!("{:?}", clusters.len());
+    println!("{:?}", processed_clusters.len());
+
+    println!(
+        "{:?}",
+        clusters.iter().max_by(|c1, c2| c1.len().cmp(&c2.len()))
+    );
+    println!(
+        "{:?}",
+        processed_clusters
+            .iter()
+            .max_by(|c1, c2| c1.len().cmp(&c2.len()))
+    );
+
+    let cluster = processed_clusters
+        .iter()
+        .max_by(|c1, c2| c1.len().cmp(&c2.len()));
+    let mut cluster = cluster.unwrap().clone();
+    cluster.sort();
+
+    println!("{:?}", cluster.join(","));
 }
